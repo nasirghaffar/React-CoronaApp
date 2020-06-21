1f449ae956226a04a2fdcc1ddd88e2dc55bde68c
Nasir Ghaffar <nasirghafar@gmail.com>
2020-06-21 17:17:21 +0100

    Updates

---
+++ precache-manifest.004e9d1353809483cf58a44ab00e32ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8b06805bd98063653916cf99c7585cf5",
+    "url": "/React-CoronaApp/index.html"
+  },
+  {
+    "revision": "4a8606d3d39bc1158424",
+    "url": "/React-CoronaApp/static/css/main.4c38b20a.chunk.css"
+  },
+  {
+    "revision": "dd8ac3c1d025858f4cf1",
+    "url": "/React-CoronaApp/static/js/2.1592418a.chunk.js"
+  },
+  {
+    "revision": "176852ae27885dfd7559e0fb4b332d24",
+    "url": "/React-CoronaApp/static/js/2.1592418a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4a8606d3d39bc1158424",
+    "url": "/React-CoronaApp/static/js/main.a90c5131.chunk.js"
+  },
+  {
+    "revision": "c3b5ae50fba3fc6cbca4",
+    "url": "/React-CoronaApp/static/js/runtime-main.30733427.js"
+  },
+  {
+    "revision": "d7265326c3a1da5ec94c50746235f734",
+    "url": "/React-CoronaApp/static/media/image.d7265326.png"
+  }
+]);
